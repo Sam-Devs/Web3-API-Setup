a240b741d02939b29f0cc444994e1ced4f3f2981
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-04 14:18:18 +0100

    web3 networks setup

---
+++ 0_network_setup/web3_setup.js
@@ -0,0 +1,22 @@
+ 
+ const Web3 = require('web3')
+
+ // ganache local blockchain
+ const ganache = 'http://127.0.0.1:7545'
+ 
+ // infura
+ const infura_mainnet = 'https://mainnet.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
+ 
+ const infura_ropsten = 'https://ropsten.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
+ 
+ // using geth
+//  const geth = 'http://127.0.0.1:8545'
+ 
+ // @ts-ignore
+//  const web3 = new Web3(ganache)
+//  const web3 = new Web3(infura_mainnet)
+//  const web3 = new Web3(infura_ropsten)
+//  const web3 = new Web3(geth)
+ 
+ 
+ console.log(web3)
