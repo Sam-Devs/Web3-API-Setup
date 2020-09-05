8fad20a44401d26374be76554bb7b79b3c5c5f90
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-05 18:36:08 +0100

    accounts setup

--- 1_accounts_setup/create_new_account.js
+++ 1_accounts_setup/create_new_account.js
@@ -2,7 +2,7 @@
 
 // infura Network
 // Mainnet
-// const infura_mainnet = 'https://mainnet.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
+const infura_mainnet = 'https://mainnet.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
 
 // Testnet => Ropsten
 // const infura_ropsten = 'https://ropsten.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
@@ -18,8 +18,7 @@
 //  const web3 = new Web3(geth)
 
 const Web3 = require('web3')
-const ganache = 'http://127.0.0.1:7545'
-const web3 = new Web3(ganache)
+const web3 = new Web3(infura_mainnet)
 
 const createAccount = () => {
     const account = web3.eth.accounts.create()
