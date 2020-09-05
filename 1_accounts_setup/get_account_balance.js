d0eabcd623e5d25710e3315053523c689ee3343f
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-05 19:51:15 +0100

    accounts setup

---
+++ 1_accounts_setup/get_account_balance.js
@@ -0,0 +1,34 @@
+// @ts-nocheck
+
+// infura Network
+// Mainnet
+const infura_mainnet = 'https://mainnet.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
+
+// Testnet => Ropsten
+// const infura_ropsten = 'https://ropsten.infura.io/v3/006328960ee54ae8a36245d0d0b3665b'
+
+// Geth
+//  const geth = 'http://127.0.0.1:8545'
+
+// @ts-ignore
+// Instances connecting to web3 with different networks
+//  const web3 = new Web3(ganache)
+//  const web3 = new Web3(infura_mainnet)
+//  const web3 = new Web3(infura_ropsten)
+//  const web3 = new Web3(geth)
+
+// const Web3 = require('web3')
+// const web3 = new Web3(infura_mainnet);
+
+// const getAccount = () => {
+//     const address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+//     try {
+//         const account = web3.eth.accounts.getBalance(address)
+//         console.log(account)
+        
+//     } catch (error) {
+        
+//     }
+// }
+
+// getAccount();
