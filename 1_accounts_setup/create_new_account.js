1a4d85e3e3c08973d5c061ab54c9cc5a4ce60081
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-05 18:11:26 +0100

    accounts setup

--- 1_accounts_setup/create_new_account.js
+++ 1_accounts_setup/create_new_account.js
@@ -21,9 +21,9 @@ const Web3 = require('web3')
 const ganache = 'http://127.0.0.1:7545'
 const web3 = new Web3(ganache)
 
-const accounts = () => {
+const createAccount = () => {
     const account = web3.eth.accounts.create()
     console.log(account)
 }
 
-accounts();
+createAccount();
