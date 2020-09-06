4c758c8bfb32912b592f5f47076acc9ce0ca5930
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-06 22:54:15 +0100

    getAccountBalance.js

--- 1_accounts_setup/get_account_balance.js
+++ 1_accounts_setup/get_account_balance.js
@@ -17,8 +17,8 @@ const infura_mainnet = 'https://mainnet.infura.io/v3/006328960ee54ae8a36245d0d0b
 //  const web3 = new Web3(infura_ropsten)
 //  const web3 = new Web3(geth)
 
-// const Web3 = require('web3')
-// const web3 = new Web3(infura_mainnet);
+const Web3 = require('web3')
+const web3 = new Web3(infura_mainnet);
 
 const address = "0x742d35cc6634c0532925a3b844bc454e4438f44e";
 
