0c8f9e60f56b52bd9e42b71a16d302cf4e160013
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-07 02:05:28 +0100

    getAccountBalance.js

--- 1_accounts_setup/get_account_balance.js
+++ 1_accounts_setup/get_account_balance.js
@@ -22,6 +22,19 @@ const web3 = new Web3(infura_mainnet);
 
 const address = "0x742d35cc6634c0532925a3b844bc454e4438f44e";
 
+// Using await/async
+const accounts = async () => {
+    try {
+        const accountBalance = await web3.eth.getBalance(address)
+        console.log(accountBalance)
+    } catch (error) {
+        console.log(error);
+    }
+}
+accounts();
+
+
+// Using a callback function
 const getAccount = () => {
     web3.eth.getBalance(address, (balance, error) => {
         if (!error) {
@@ -29,16 +42,6 @@ const getAccount = () => {
         } else {
             console.log(error);
         }
-
-        // Using try and catch returns 
-        // Error: Please pass numbers as strings or BN objects to avoid precision errors.
-        // ****Note**** This will be fixed 
-        // try {
-        //     const balance = web3.utils.fromWei(parseInt(account),"ether")
-        //     console.log(balance);
-        // } catch (error) {
-        //     console.log(error);
-        // }
     })
 }
 
