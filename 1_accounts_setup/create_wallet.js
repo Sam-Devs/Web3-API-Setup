f8809b54f5b4fe9fe787f4e1394b9a4be9e2c62b
Sam Ayorinde <ayorindesam2@gmail.com>
2020-09-08 01:48:10 +0100

    createWallet.js

--- 1_accounts_setup/create_wallet.js
+++ 1_accounts_setup/create_wallet.js
@@ -20,10 +20,9 @@ const infura_mainnet = 'https://mainnet.infura.io/v3/006328960ee54ae8a36245d0d0b
 const Web3 = require('web3')
 const web3 = new Web3(infura_mainnet);
 
-const address = "0x742d35cc6634c0532925a3b844bc454e4438f44e";
 
 // Using await/async
-const accounts = async () => {
+const createWallet = async () => {
     try {
         const accountBalance = await web3.eth.getBalance(address)
         console.log(accountBalance)
